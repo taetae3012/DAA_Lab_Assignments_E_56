691fe0580417b3c7a45c7980e5a0832774f37f15
Sristi <sristi1312@gmail.com>
2021-08-03 09:55:01 +0530

    second attempt to binarysearch

--- week1/src/daa/BinarySearch.java
+++ week1/src/daa/BinarySearch.java
@@ -1,44 +1,53 @@
 package daa;
 import java.util.Scanner;
+//Program for the binary search algorithm
+//algo is right working problem is in the main loop
+//firstly committed the program week2 remainning
+//whole thing is not pushed to the github.
 
 public class BinarySearch {
 	
-	public static void binarysearch(int arr[],int first,int last,int key) {
-		int mid=(first+last)/2;
-		while(first<=last) {
-			if(arr[mid]<key) {
-				first=mid+1;
-			}else if(arr[mid]==key) {
-				System.out.println("Present");
-				break;
-			}else {
-				last=mid-1;
+	int binarysearch(int arr[],int l,int r,int x) {
+		if(r>=l) {
+			int mid =1+(r-l)/2;
+			if(arr[mid]==x) {
+				return mid;
 			}
-			mid=(first+last)/2;
+			if(arr[mid]>x) {
+				return binarysearch(arr,l,mid-1,x);
+			}
+			return binarysearch(arr,mid+1,r,x);
 		}
-		if(first>last) {
-			System.out.println("Not Present");
-		}
+		return -1;
 	}
 	
 	
    public static void main(String[] args) {
-	 /*  Scanner sc=new Scanner(System.in);
+	   BinarySearch ob=new BinarySearch();
+	   Scanner sc=new Scanner(System.in);
 	   int test=sc.nextInt();
-	   int arr[]=new int[40];
+	   
 	   for(int i=0;i<test;i++) {
-		   int size=sc.nextInt();
-		   for(int j=0;j<size;j++) {
-		   arr[i]=sc.nextInt(); 
+		   int n=sc.nextInt();
+		   int arr[]=new int[n];
+		   for(int j=0;j<n;j++) {
+			   arr[j]=sc.nextInt();
+		   }
+		   int x=sc.nextInt();
+		   int result=ob.binarysearch(arr,0,n-1,x);
+		   if(result==-1) {
+			   System.out.println("Not Present");
+		   }
+		   else {
+			   System.out.println("Present"+result);
+		   } 
+		   
 	   }
-		   int first=0;
-		   int last=arr.length-1;
-		   int key=sc.nextInt();
-		binarysearch(arr,first,last,key);
-	   }
-	*/
-	   int arr[]= {1,23,45,78};
-	   binarysearch(arr,0,3,78);
+			   
+	 
+	   
+	  
+	
 	   
 }
 }
